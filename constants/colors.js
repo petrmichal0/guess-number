9b9123005f2512a76aa366f4aec5a5066c6bdd91
Petr Michal <petrmichal0@gmail.com>
2024-10-14 20:28:26 +0200

    DELETE notes

--- constants/colors.js
+++ constants/colors.js
@@ -1,6 +1,6 @@
 export const colors = {
-  primary500: "#FFBB33", // zářivá oranžová
-  primary600: "#FF8800", // tmavší oranžová
-  background: "#2E2E2E", // tmavě šedé pozadí
-  title: "#F0F0F0", // světle šedá pro text
+  primary500: "#FFBB33",
+  primary600: "#FF8800",
+  background: "#2E2E2E",
+  title: "#F0F0F0",
 };
